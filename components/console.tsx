ddc57b23937a7a1044fd481680d782b1c322b68a
florencia <anniceflorencia@gmail.com>
2025-11-03 12:01:54 +0300

    add animated list for sigle test in editor

--- components/console.tsx
+++ components/console.tsx
@@ -1,6 +1,9 @@
 import { ArrowBigDownDash, CloudCheck, File, Play } from 'lucide-react'
 import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from './ui/select';
 import { CODE_SNIPPETS } from '@/lib/constant';
+import AnimatedList from './AnimatedList';
+
+const items = ['Test 1', 'Test 2', 'Test 3', 'Test 4', 'Test 5', 'Test 6', 'Test 7', 'Test 8', 'Test 9', 'Test 10']; 
 
 export default function Console({setLanguage, setValue}: {setLanguage: (lang: string) => void, setValue: (code: string) => void}) {
   return (
@@ -40,10 +43,19 @@ export default function Console({setLanguage, setValue}: {setLanguage: (lang: st
             <p className='my-1 text-gray-200'>Output: </p>
             {/* Console Output Area */}
         </div>
-        <div className="h-[30vh] w-full bg-neutral-800/30 mt-2 rounded-lg p-4 text-sm text-green-400 font-mono overflow-y-auto">
-        {/* Console Output Area */}
-            <p className='my-2 text-gray-200'>Test: </p>
+        <div className="h-[30vh] border border-neutral-800 w-full mt-2 rounded-sm text-sm text-green-400 font-mono overflow-hidden">
+            {/* list of test */}
+            <AnimatedList
+                items={items}
+                onItemSelect={(item, index) => alert(item+ index)}
+                showGradients={true}
+                enableArrowNavigation={true}
+                displayScrollbar={true}
+            />
         </div>
+        <button className='mt-2 cursor-pointer bg-amber-600 hover:bg-amber-700 transition-all ease-in-out duration-200 rounded-lg w-full py-2 px-4 text-center'>
+            Finish
+        </button>
     </div>
   )
 }
