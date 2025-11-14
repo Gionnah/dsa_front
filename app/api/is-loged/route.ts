ed6a96431d0294e4bfaf561aa2a44235fe8d1456
florencia <anniceflorencia@gmail.com>
2025-11-14 16:16:42 +0300

    FIX BUG and change design login

---
+++ app/api/is-loged/route.ts
@@ -0,0 +1,14 @@
+// /api/is-loged/route.ts
+
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function GET(req: NextRequest)
+{
+    const access = await req.cookies.get('Access');
+    const refresh = await req.cookies.get('Refresh');
+
+    if (access && refresh)
+        return NextResponse.json({ok: 'ok'}, {status: 200})
+    return NextResponse.json({ok: ''}, {status: 400})
+}
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,24 +4,26 @@ import { useEffect, useState } from 'react'
 
 export default function Login() {
   const [loading, setLoading] = useState(false);
-  const [isLoged, setIsLoged] = useState(false);
+  const [isLoged, setIsLoged] = useState(true);
 
   const verifyLog = async () => {
       setLoading(true);
-      const res = await fetch('/api/me');
-      if (res.ok) window.location.href = '/home';
+      const res = await fetch('/api/is-loged');
+      if (res.ok) 
+        window.location.href = '/home';
+      else
+        setIsLoged(false);
       setLoading(false);
-      setIsLoged(true);
   }
 
   useEffect(() => {
     verifyLog();
-  }, [isLoged, loading])
+  }, [])
   if (loading)
     return (
       <div> loading... </div>
     );
-  if (isLoged)
+  if (!isLoged)
     return (
       <div className='h-screen w-full bg-indigo-50'>
           <LoginForm />
