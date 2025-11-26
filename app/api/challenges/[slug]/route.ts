43195dd778fab41e956ec9eb7fd599487f6d1259
florencia <anniceflorencia@gmail.com>
2025-11-26 10:25:16 +0300

    edit promise in challenge slugc

--- app/api/challenges/[slug]/route.ts
+++ app/api/challenges/[slug]/route.ts
@@ -1,30 +1,40 @@
-// /api/challenges/route.ts
+// /api/challenges/[slug]/route.ts
 import { NextRequest, NextResponse } from "next/server";
 
 const URL = process.env.API_URL;
 
-export async function GET(req: NextRequest, {  params}: {params: {slug: string}}) {
+export async function GET(
+  req: NextRequest, 
+  { params }: { params: Promise<{ slug: string }> }
+) {
     const { slug } = await params; 
-    const response = await fetch(`${URL}/challenges/${slug}` , {
+    
+    const response = await fetch(`${URL}/challenges/${slug}`, {
         method: "GET",
         headers: {
             "Authorization": `Bearer ${req.cookies.get('Access')?.value || ''}`,
             "Content-Type": "application/json",
         },
     });
+    
     const data = await response.json();
     return NextResponse.json(data);
 }
 
-export async function POST(req: NextRequest, {  params}: {params: {slug: string}}) {
+export async function POST(
+  req: NextRequest, 
+  { params }: { params: Promise<{ slug: string }> }
+) {
     const { slug } = await params; 
-    const response = await fetch(`${URL}/challenges/${slug}/join/` , {
+    
+    const response = await fetch(`${URL}/challenges/${slug}/join/`, {
         method: "POST",
         headers: {
             "Authorization": `Bearer ${req.cookies.get('Access')?.value || ''}`,
             "Content-Type": "application/json",
         },
     });
+    
     const data = await response.json();
     return NextResponse.json(data);
 }
