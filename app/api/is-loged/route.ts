d24f6aac6624422e89ff1797490848283b8d3edf
florencia <anniceflorencia@gmail.com>
2025-11-26 12:11:42 +0300

    secure falses

--- app/api/is-loged/route.ts
+++ app/api/is-loged/route.ts
@@ -10,5 +10,5 @@ export async function GET(req: NextRequest)
 
     if (access && refresh)
         return NextResponse.json({ok: 'ok'}, {status: 200})
-    return NextResponse.json({ok: ''}, {status: 200})
+    return NextResponse.json({ok: ''}, {status: 400})
 }
