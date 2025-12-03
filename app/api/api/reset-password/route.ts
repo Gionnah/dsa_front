a2615843bc71bc337a4ab8148be9071732552933
florencia <anniceflorencia@gmail.com>
2025-12-03 14:57:44 +0300

    comment content for route reset passwordr

--- app/api/api/reset-password/route.ts
+++ app/api/api/reset-password/route.ts
@@ -2,15 +2,15 @@ import { NextRequest, NextResponse } from "next/server";
 
 export default async function POST(req: NextRequest)
 {
-    const body = await req.json();
+    // const body = await req.json();
 
-    const res = await fetch(`${process.env.API_URL}/reset-password/`,
-        {
-            method: 'POST',
-            body
-        }
-    )
-    if (!res.ok)
-        return NextResponse.json({ok: ''});
+    // const res = await fetch(`${process.env.API_URL}/reset-password/`,
+    //     {
+    //         method: 'POST',
+    //         body
+    //     }
+    // )
+    // if (!res.ok)
+    //     return NextResponse.json({ok: ''});
     return NextResponse.json({ok: 'Ok!'})
 }
