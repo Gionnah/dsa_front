7e9663563488540e6719b8218d023d40a515ebab
vmgionah@gmail.com <vmgionah@gmail.>
2026-08-05 11:46:19 +0300

    edit markdown 2

---
+++ src/types/katex.d.ts
@@ -0,0 +1,10 @@
+// src/types/katex.d.ts
+declare module 'katex/dist/katex.min.css' {
+    const content: string;
+    export default content;
+}
+
+declare module 'katex/dist/katex.css' {
+    const content: string;
+    export default content;
+}
